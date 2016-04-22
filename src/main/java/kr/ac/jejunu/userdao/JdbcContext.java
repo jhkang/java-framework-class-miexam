252557bbc84f0266a91f88b8d7f5bf7678096838
JinHyeok Kang <jinhyeok.kang@gmail.com>
2016-04-22 17:40:17 +0900

    JdbcContext 로 Refactoring

---
+++ src/main/java/kr/ac/jejunu/userdao/JdbcContext.java
@@ -0,0 +1,126 @@
+package kr.ac.jejunu.userdao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcContext {
+    ConnectionMaker connectionMaker;
+
+    public JdbcContext() {
+    }
+
+    User jdbcWithStatementForGet(StatementStrategy statementStrategy) throws SQLException, ClassNotFoundException {
+        Connection connection = connectionMaker.getConnection();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user = null;
+        try {
+            preparedStatement = statementStrategy.makeStatement(connection);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return user;
+    }
+
+    Long jdbcWithStatementForAdd(StatementStrategy statementStrategy) throws ClassNotFoundException, SQLException {
+        Connection connection = connectionMaker.getConnection();
+        PreparedStatement preparedStatement = null;
+        Long id = null;
+        try {
+            preparedStatement = statementStrategy.makeStatement(connection);
+            preparedStatement.executeUpdate();
+
+            id = getLastInsertId(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return id;
+    }
+
+    void jdbcWithStatementForUpdate(StatementStrategy statementStrategy) throws SQLException, ClassNotFoundException {
+        Connection connection = connectionMaker.getConnection();
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = statementStrategy.makeStatement(connection);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    Long getLastInsertId(Connection connection) throws SQLException, ClassNotFoundException {
+        PreparedStatement preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        return resultSet.getLong(1);
+    }
+
+    public void setConnectionMaker(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+}
--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -3,141 +3,31 @@ package kr.ac.jejunu.userdao;
 import java.sql.*;
 
 public class UserDao {
-    private ConnectionMaker connectionMaker;
+    private JdbcContext jdbcContext;
 
     public User get(Long id) throws ClassNotFoundException, SQLException {
         StatementStrategy statementStrategy = new GetStatementStrategy(id);
-        User user = jdbcWithStatementForGet(statementStrategy);
+        User user = jdbcContext.jdbcWithStatementForGet(statementStrategy);
         return user;
     }
 
     public Long add(User user) throws ClassNotFoundException, SQLException {
         StatementStrategy statementStrategy = new AddStatementStrategy(user);
-        Long id = jdbcWithStatementForAdd(statementStrategy);
+        Long id = jdbcContext.jdbcWithStatementForAdd(statementStrategy);
         return id;
     }
 
     public void update(User user) throws SQLException, ClassNotFoundException {
         StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
-        jdbcWithStatementForUpdate(statementStrategy);
+        jdbcContext.jdbcWithStatementForUpdate(statementStrategy);
     }
 
     public void delete(Long id) throws SQLException, ClassNotFoundException {
         StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
-        jdbcWithStatementForUpdate(statementStrategy);
+        jdbcContext.jdbcWithStatementForUpdate(statementStrategy);
     }
 
-    private User jdbcWithStatementForGet(StatementStrategy statementStrategy) throws SQLException, ClassNotFoundException {
-        Connection connection = connectionMaker.getConnection();
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        User user = null;
-        try {
-            preparedStatement = statementStrategy.makeStatement(connection);
-            resultSet = preparedStatement.executeQuery();
-
-            if(resultSet.next()) {
-                user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setPassword(resultSet.getString("password"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
-        } finally {
-            if(resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return user;
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
     }
-
-    private Long jdbcWithStatementForAdd(StatementStrategy statementStrategy) throws ClassNotFoundException, SQLException {
-        Connection connection = connectionMaker.getConnection();
-        PreparedStatement preparedStatement = null;
-        Long id = null;
-        try {
-            preparedStatement = statementStrategy.makeStatement(connection);
-            preparedStatement.executeUpdate();
-
-            id = getLastInsertId(connection);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
-        } finally {
-            if(preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return id;
-    }
-
-    private void jdbcWithStatementForUpdate(StatementStrategy statementStrategy) throws SQLException, ClassNotFoundException {
-        Connection connection = connectionMaker.getConnection();
-        PreparedStatement preparedStatement = null;
-        try {
-            preparedStatement = statementStrategy.makeStatement(connection);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw e;
-        } finally {
-            if(preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if(connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    private Long getLastInsertId(Connection connection) throws SQLException, ClassNotFoundException {
-        PreparedStatement preparedStatement = connection.prepareStatement("select last_insert_id()");
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        return resultSet.getLong(1);
-    }
-
-    public void setConnectionMaker(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
-    }
-
 }
